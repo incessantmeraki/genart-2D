6bf0f5454e401830aac6fb9b6b45e16f7deea5c5
Ashwin Kandel <incessantmeraki@gmail.com>
2018-12-02 15:53:03 +1100

    Add rotation and fill text

--- sketch.js
+++ sketch.js
@@ -13,15 +13,17 @@ const sketch = () => {
   
   const createGrid = () => {
     const points = [];
-    const count = 30;
+    const count = 40;
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
+        const radius = Math.abs(random.noise2D(u,v)) * .2;
         points.push({
+          rotation: random.noise2D(u,v),
           color: random.pick(palette),
           position: [u, v],
-          radius: Math.abs(0.01 + random.gaussian() * 0.01)
+          radius
         });
       }
     }
@@ -30,7 +32,7 @@ const sketch = () => {
 
   // random.setSeed(500)
   const points = createGrid().filter(() => random.value() > 0.5);
-  const margin = 200;
+  const margin = 400;
 
   return ({ context, width, height }) => {
     context.fillStyle = "white";
@@ -38,16 +40,24 @@ const sketch = () => {
 
     points.forEach(data => {
       
-      const { position, radius, color } = data;
+      const { position, radius, color, rotation } = data;
       const [u, v] = position;
       
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, width - margin, v);
 
-      context.beginPath();
-      context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      // context.beginPath();
+      // context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      // context.fillStyle = color;
+      // context.fill();
+
+      context.save()
       context.fillStyle = color;
-      context.fill();
+      context.font = `${radius * width}px "Helvetica"`
+      context.translate(x,y)
+      context.rotate(rotation)
+      context.fillText('=', 0, 0)
+      context.restore()
     });
   };
 };
