93bb26b9d967e61d69eb0364e208ceac3e799f7c
Ashwin Kandel <incessantmeraki@gmail.com>
2018-12-02 14:56:17 +1100

    introduce gaussian randomness

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
-const canvasSketch = require('canvas-sketch');
-const { lerp } = require('canvas-sketch-util/math')
+const canvasSketch = require("canvas-sketch");
+const { lerp } = require("canvas-sketch-util/math");
+const random = require("canvas-sketch-util/random");
 
 const settings = {
   dimensions: [2048, 2048]
@@ -8,33 +9,40 @@ const settings = {
 const sketch = () => {
   const createGrid = () => {
     const points = [];
-    const count = 5;
-    for (let x = 0 ; x < count ; x++) {
-      for (let y = 0; y < count ; y++) {
-        const u = count <=1 ? 0.5 :x / (count -1) ;
-        const v = count <=1 ? 0.5 :y / (count -1 );
-        points.push([u,v]);
+    const count = 20;
+    for (let x = 0; x < count; x++) {
+      for (let y = 0; y < count; y++) {
+        const u = count <= 1 ? 0.5 : x / (count - 1);
+        const v = count <= 1 ? 0.5 : y / (count - 1);
+        points.push({
+          position: [u, v],
+          radius: Math.abs(0.01 + random.gaussian() * 0.01)
+        });
       }
     }
     return points;
-  }
+  };
 
-  const points = createGrid();
-  const margin  = 400;
+  // random.setSeed(500)
+  const points = createGrid().filter(() => random.value() > 0.5);
+  console.log(points);
+  const margin = 200;
+
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+    context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
-    points.forEach(([u,v]) => {
-      const x = lerp( margin, width-margin, u)
-      const y = lerp( margin, width-margin, v)
+    points.forEach(data => {
+      const { position, radius } = data;
+      const [u, v] = position;
+      const x = lerp(margin, width - margin, u);
+      const y = lerp(margin, width - margin, v);
 
       context.beginPath();
-      context.arc(x,y,100,0,Math.PI*2,false);
-      context.strokeStyle='black';
-      context.lineWidth= 40
-      context.stroke();
-    })
+      context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      context.fillStyle = "red";
+      context.fill();
+    });
   };
 };
 
